6845073b0e07f0a686c30535b84a7fa8751289bc
Kanbo <Kanbo@DESKTOP-814S7AG>
2025-10-09 16:52:13 +0200

    CampaignInfo no longer requires a hero Clans on gold/influence debt/0 will get 25k gold/100 influence. Weekly check

--- BannerlordTwitch/BLTAdoptAHero/Actions/CampaignInfo.cs
+++ BannerlordTwitch/BLTAdoptAHero/Actions/CampaignInfo.cs
@@ -9,28 +9,29 @@ using BannerlordTwitch.Util;
 using BannerlordTwitch.Helpers;
 using JetBrains.Annotations;
 using TaleWorlds.CampaignSystem;
-using TaleWorlds.CampaignSystem.Actions;
 using TaleWorlds.CampaignSystem.Settlements;
-using TaleWorlds.Core;
-using TaleWorlds.Library;
 using TaleWorlds.Localization;
-using TaleWorlds.MountAndBlade;
-
 
 namespace BLTAdoptAHero
 {
     [LocDisplayName("{=TESTING}CampaignInfo"),
      LocDescription("{=TESTING}Shows kingdom list, culture list, wars list and specific kingdom/war info"),
      UsedImplicitly]
-    public class CampaignInfo : HeroCommandHandlerBase
+    public class CampaignInfo : ICommandHandler
     {
-        protected override void ExecuteInternal(Hero adoptedHero, ReplyContext context, object config,
-            Action<string> onSuccess, Action<string> onFailure)
+        public Type HandlerConfigType => null;
+
+        public void Execute(ReplyContext context, object config)
+        {
+            // hero not required
+            ExecuteInternal(context, config);
+        }
+
+        private void ExecuteInternal(ReplyContext context, object config)
         {
             if (string.IsNullOrWhiteSpace(context.Args))
             {
-                ActionManager.SendReply(context,
-                    context.ArgsErrorMessage("{=TESTING}invalid mode (use kingdomlist, culturelist, warlist, kingdom (kingdom), war (kingdom)".Translate()));
+                ActionManager.SendReply(context, "{=TESTING}invalid mode (use kingdomlist, culturelist, warlist, kingdom (kingdom), war (kingdom))".Translate());
                 return;
             }
 
@@ -41,167 +42,149 @@ namespace BLTAdoptAHero
             switch (mode)
             {
                 case "kingdomlist":
-                    {
-                        string kingdomList = string.Join(", ", CampaignHelpers.MainFactions.Select(c => c.Name.ToString()));
-                        onSuccess(kingdomList);
-                        break;
-                    }
+                    ActionManager.SendReply(context,
+                        string.Join(", ", CampaignHelpers.MainFactions.Select(c => c.Name.ToString())));
+                    break;
 
                 case "culturelist":
-                    {
-                        string cultureList = string.Join(", ", CampaignHelpers.MainCultures.Select(c => c.Name.ToString()));
-                        onSuccess(cultureList);
-                        break;
-                    }
+                    ActionManager.SendReply(context,
+                        string.Join(", ", CampaignHelpers.MainCultures.Select(c => c.Name.ToString())));
+                    break;
 
                 case "kingdom":
-                    {
-                        if (string.IsNullOrWhiteSpace(desiredName))
-                        {
-                            onFailure("{=TESTING}Need kingdom name".Translate());
-                        }
-                        var desiredKingdom = CampaignHelpers.AllHeroes.Select(h => h?.Clan?.Kingdom).Distinct().FirstOrDefault(c => c?.Name.ToString().Equals(desiredName, StringComparison.OrdinalIgnoreCase) == true);
-                        if (desiredKingdom == null)
-                        {
-                            onFailure("{=JdZ2CelP}Could not find the kingdom with the name {name}".Translate(("name", desiredName)));
-                            return;
-                        }
-                        else
-                        {
-                            bool war = false;
-                            TextObject warList = new TextObject();
-                            foreach (Kingdom k in Kingdom.All)
-                            {
-                                if ((desiredKingdom != k) && (desiredKingdom.IsAtWarWith(k)))
-                                {
-                                    war = true;
-                                    warList.Value = warList.Value + k.Name.Value + ":" + ((int)k.TotalStrength).ToString() + ", ";
-                                }
-                            }
-                            warList.Value = warList.Value.TrimEnd(',', ' ');
-
-                            var clanStats = new StringBuilder();
-                            clanStats.Append("{=SVlrGgol}Kingdom Name: {name} | ".Translate(("name", desiredKingdom.Name.ToString())));
-                            clanStats.Append("{=Ss588M9l}Ruling Clan: {rulingClan} | ".Translate(("rulingClan", desiredKingdom.RulingClan.Name.ToString())));
-                            clanStats.Append("{=T1FhhCH9}Clan Count: {clanCount} | ".Translate(("clanCount", desiredKingdom.Clans.Count.ToString())));
-                            clanStats.Append("{=TUOmh7NY}Strength: {strength} | ".Translate(("strength", Math.Round(desiredKingdom.TotalStrength).ToString())));                            
-                            if (war)
-                                clanStats.Append("{=QadZnUKh}Wars: {wars} | ".Translate(("wars", warList.ToString())));
-                            if (desiredKingdom.RulingClan.HomeSettlement.Name != null)
-                                clanStats.Append("{=EXKsUpaU}Capital: {capital} | ".Translate(("capital", desiredKingdom.RulingClan.HomeSettlement.Name.ToString())));
-                            if (desiredKingdom.Fiefs.Count >= 1)
-                            {
-                                int townCount = 0;
-                                int castleCount = 0;
-                                foreach (var settlement in desiredKingdom.Fiefs)
-                                {
-                                    if (!settlement.IsCastle)
-                                    {
-                                        townCount++;
-                                    }
-                                    if (settlement.IsCastle)
-                                    {
-                                        castleCount++;
-                                    }
-                                }
-                                clanStats.Append("{=BwuFSJU1}Towns: {towns} | ".Translate(("towns", (object)townCount)));
-                                clanStats.Append("{=0rMNNQ7R}Castles: {castles}".Translate(("castles", (object)castleCount)));
-                            }
-                            onSuccess("{stats}".Translate(("stats", clanStats.ToString())));
-                        }
-
-                        break;
-                    }
+                    ShowKingdom(desiredName, context);
+                    break;
 
                 case "warlist":
-                    {
-                        List<string> kingdomId = new List<string>();
-                        var warList = new StringBuilder();
-                        for (int i = 0; i < Kingdom.All.Count; i++)
-                        {
-                            for (int j = 0; j < Kingdom.All.Count; j++)
-                            {
-                                if ((j != i) && !kingdomId.Contains(Kingdom.All[j].StringId))
-                                {
-                                    if (Kingdom.All[i].IsAtWarWith(Kingdom.All[j]))
-                                    {
-                                        warList.Append($"{Kingdom.All[i].Name}({Math.Round(Kingdom.All[i].TotalStrength)}) VS {Kingdom.All[j].Name}({Math.Round(Kingdom.All[j].TotalStrength)}) | ");
-                                    }
-
-                                }
-
-                            }
-                            kingdomId.Add(Kingdom.All[i].StringId);
-                        }
-                        warList.ToString().TrimEnd('|', ' ');
-                        onSuccess($"{warList}");
-
-                        break;
-                    }
+                    ShowWarList(context);
+                    break;
 
                 case "war":
-                    {
-                        if (string.IsNullOrWhiteSpace(desiredName))
-                        {
-                            onFailure("{=TESTING}Need kingdom name".Translate());
-                        }
-                        var desiredKingdom = CampaignHelpers.AllHeroes.Select(h => h?.Clan?.Kingdom).Distinct().FirstOrDefault(c => c?.Name.ToString().Equals(desiredName, StringComparison.OrdinalIgnoreCase) == true);
-                        if (desiredKingdom == null)
-                        {
-                            onFailure("{=JdZ2CelP}Could not find the kingdom with the name {name}".Translate(("name", desiredName)));
-                            return;
-                        }
-                        if (!Kingdom.All.Any(k => k.IsAtWarWith(desiredKingdom)))
-                        {
-                            onFailure("{=TESTING}{kingdom} is not at war".Translate(("kingdom", desiredKingdom.Name.ToString())));
-                            return;
-                        }
-                        else
-                        {
-                            var warStats = new StringBuilder();
+                    ShowWar(desiredName, context);
+                    break;
 
-                            foreach (Kingdom k in Kingdom.All)
-                            {
-                                if (desiredKingdom != k && desiredKingdom.IsAtWarWith(k))
-                                {
-                                    StanceLink stance = desiredKingdom.GetStanceWith(k);
-
-                                    int ourCasualties = stance.GetCasualties(desiredKingdom);
-                                    int enemyCasualties = stance.GetCasualties(k);
-                                    int ourRaids = stance.GetSuccessfulRaids(desiredKingdom);
-                                    int enemyRaids = stance.GetSuccessfulRaids(k);
-                                    int ourSieges = stance.GetSuccessfulSieges(desiredKingdom);
-                                    int enemySieges = stance.GetSuccessfulSieges(k);
-
-                                    warStats.Append("{=TESTING}{kingdom1} VS {kingdom2} = Casualties: {c1}/{c2} - Raids: {r1}/{r2} - Sieges: {s1}/{s2} - Started: {date}"
-                                        .Translate(
-                                            ("kingdom1", desiredKingdom.Name.ToString()),
-                                            ("kingdom2", k.Name.ToString()),
-                                            ("c1", ourCasualties.ToString()),
-                                            ("c2", enemyCasualties.ToString()),
-                                            ("r1", ourRaids.ToString()),
-                                            ("r2", enemyRaids.ToString()),
-                                            ("s1", ourSieges.ToString()),
-                                            ("s2", enemySieges.ToString()),
-                                            ("date", stance.WarStartDate.ToString())
-                                        ));
-
-                                    warStats.Append(" | "); // separator between wars
-                                }
-                            }
-
-                            string result = warStats.ToString().TrimEnd('|', ' ');
-                            onSuccess(result);
-                        }
-
-                       break;
-                    }
                 default:
-                    {
-                        onFailure("{=TESTING}invalid mode (use kingdomlist, culturelist, warlist, kingdom (kingdom), war (kingdom)".Translate());
-                        break; 
-                    }
+                    ActionManager.SendReply(context,
+                        "{=TESTING}invalid mode (use kingdomlist, culturelist, warlist, kingdom (kingdom), war (kingdom))".Translate());
+                    break;
             }
+        }
+
+        private void ShowKingdom(string desiredName, ReplyContext context)
+        {
+            if (string.IsNullOrWhiteSpace(desiredName))
+            {
+                ActionManager.SendReply(context, "{=TESTING}Need kingdom name".Translate());
+                return;
+            }
+
+            var desiredKingdom = Kingdom.All.FirstOrDefault(c =>
+                c?.Name.ToString().Equals(desiredName, StringComparison.OrdinalIgnoreCase) == true);
+
+            if (desiredKingdom == null)
+            {
+                ActionManager.SendReply(context,
+                    "{=JdZ2CelP}Could not find the kingdom with the name {name}".Translate(("name", desiredName)));
+                return;
+            }
+
+            bool war = false;
+            var warList = new StringBuilder();
+            foreach (Kingdom k in Kingdom.All)
+            {
+                if (desiredKingdom != k && desiredKingdom.IsAtWarWith(k))
+                {
+                    war = true;
+                    warList.Append($"{k.Name}:{(int)k.TotalStrength}, ");
+                }
+            }
+            if (war) warList.Length -= 2;
+
+            var sb = new StringBuilder();
+            sb.Append("{=SVlrGgol}Kingdom Name: {name} | ".Translate(("name", desiredKingdom.Name.ToString())));
+            sb.Append("{=Ss588M9l}Ruling Clan: {rulingClan} | ".Translate(("rulingClan", desiredKingdom.RulingClan.Name.ToString())));
+            sb.Append("{=T1FhhCH9}Clan Count: {count} | ".Translate(("count", desiredKingdom.Clans.Count)));
+            sb.Append("{=TUOmh7NY}Strength: {strength} | ".Translate(("strength", Math.Round(desiredKingdom.TotalStrength))));
+            if (war)
+                sb.Append("{=QadZnUKh}Wars: {wars} | ".Translate(("wars", warList.ToString())));
+            if (desiredKingdom.RulingClan.HomeSettlement != null)
+                sb.Append("{=EXKsUpaU}Capital: {capital} | ".Translate(("capital", desiredKingdom.RulingClan.HomeSettlement.Name.ToString())));
+
+            int towns = desiredKingdom.Fiefs.Count(f => !f.IsCastle);
+            int castles = desiredKingdom.Fiefs.Count(f => f.IsCastle);
+            sb.Append("{=BwuFSJU1}Towns: {towns} | ".Translate(("towns", towns)));
+            sb.Append("{=0rMNNQ7R}Castles: {castles}".Translate(("castles", castles)));
+
+            ActionManager.SendReply(context, sb.ToString());
+        }
+
+        private void ShowWarList(ReplyContext context)
+        {
+            var seen = new HashSet<string>();
+            var sb = new StringBuilder();
+            foreach (var k1 in Kingdom.All)
+            {
+                foreach (var k2 in Kingdom.All)
+                {
+                    if (k1 == k2 || seen.Contains(k2.StringId))
+                        continue;
+
+                    if (k1.IsAtWarWith(k2))
+                        sb.Append($"{k1.Name}({Math.Round(k1.TotalStrength)}) VS {k2.Name}({Math.Round(k2.TotalStrength)}) | ");
+                }
+                seen.Add(k1.StringId);
+            }
+            ActionManager.SendReply(context, sb.ToString().TrimEnd('|', ' '));
+        }
+
+        private void ShowWar(string desiredName, ReplyContext context)
+        {
+            if (string.IsNullOrWhiteSpace(desiredName))
+            {
+                ActionManager.SendReply(context, "{=TESTING}Need kingdom name".Translate());
+                return;
+            }
+
+            var desiredKingdom = Kingdom.All.FirstOrDefault(c =>
+                c?.Name.ToString().Equals(desiredName, StringComparison.OrdinalIgnoreCase) == true);
+
+            if (desiredKingdom == null)
+            {
+                ActionManager.SendReply(context,
+                    "{=JdZ2CelP}Could not find the kingdom with the name {name}".Translate(("name", desiredName)));
+                return;
+            }
+
+            if (!Kingdom.All.Any(k => k.IsAtWarWith(desiredKingdom)))
+            {
+                ActionManager.SendReply(context,
+                    "{=TESTING}{kingdom} is not at war".Translate(("kingdom", desiredKingdom.Name.ToString())));
+                return;
+            }
+
+            var sb = new StringBuilder();
+            foreach (var k in Kingdom.All)
+            {
+                if (desiredKingdom != k && desiredKingdom.IsAtWarWith(k))
+                {
+                    var stance = desiredKingdom.GetStanceWith(k);
+                    sb.Append("{=TESTING}{k1} VS {k2} = Casualties: {c1}/{c2} - Raids: {r1}/{r2} - Sieges: {s1}/{s2} - Started: {date}"
+                        .Translate(
+                            ("k1", desiredKingdom.Name.ToString()),
+                            ("k2", k.Name.ToString()),
+                            ("c1", stance.GetCasualties(desiredKingdom)),
+                            ("c2", stance.GetCasualties(k)),
+                            ("r1", stance.GetSuccessfulRaids(desiredKingdom)),
+                            ("r2", stance.GetSuccessfulRaids(k)),
+                            ("s1", stance.GetSuccessfulSieges(desiredKingdom)),
+                            ("s2", stance.GetSuccessfulSieges(k)),
+                            ("date", stance.WarStartDate.ToString())
+                        ));
+                    sb.Append(" | ");
+                }
+            }
+
+            ActionManager.SendReply(context, sb.ToString().TrimEnd('|', ' '));
         }
     }
 }
--- BannerlordTwitch/BLTAdoptAHero/Behaviors/BLTClanBehavior.cs
+++ BannerlordTwitch/BLTAdoptAHero/Behaviors/BLTClanBehavior.cs
@@ -14,10 +14,12 @@ namespace BLTAdoptAHero
         public BLTMarriageBehavior MarriageBehavior { get; } = new BLTMarriageBehavior();
         private BLTFamily _bltFamily;
         private CampaignTime _lastFamilyInitTime;
+        public BLTSocialSecurity SocialSecurity { get; } = new BLTSocialSecurity();
 
         public override void RegisterEvents()
         {
             MarriageBehavior.RegisterEvents();
+            SocialSecurity.RegisterEvents();
             CampaignEvents.DailyTickEvent.AddNonSerializedListener(this, () =>
             {
                 if (_bltFamily == null)
@@ -40,7 +42,7 @@ namespace BLTAdoptAHero
         }
         public override void SyncData(IDataStore dataStore)
         {
-            MarriageBehavior.SyncData(dataStore);
+
         }
 
         public class BLTMarriageBehavior
@@ -50,10 +52,6 @@ namespace BLTAdoptAHero
             {
                 CampaignEvents.OnHeroChangedClanEvent.AddNonSerializedListener(this, OnHeroChangedClanEvent);
                 CampaignEvents.HeroesMarried.AddNonSerializedListener(this, OnHeroesMarried);
-            }
-            public void SyncData(IDataStore dataStore)
-            {
-
             }
             private void OnHeroChangedClanEvent(Hero hero, Clan oldClan)
             {
@@ -196,6 +194,31 @@ namespace BLTAdoptAHero
             {
                 public Hero BltHero { get; set; }
                 public List<Hero> FamilyMembers { get; set; } = new List<Hero>();
+            }
+        }
+
+        public class BLTSocialSecurity
+        {
+            public void RegisterEvents()
+            {
+                CampaignEvents.WeeklyTickEvent.AddNonSerializedListener(this, OnWeeklyTick);
+            }
+            public void OnWeeklyTick()
+            {
+                foreach (var clan in Clan.All)
+                {
+                    if (clan.Leader.IsAdopted())
+                    {
+                        if (clan.Leader.Gold <= 0)
+                        {
+                            clan.Leader.Gold = 25000;
+                        }
+                        if (clan.Influence <= 0 && !clan.IsUnderMercenaryService && clan.Kingdom != null)
+                        {
+                            clan.Influence = 100f;
+                        }
+                    }
+                }
             }
         }
     }
