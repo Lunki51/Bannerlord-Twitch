562191a87299e67f8bfdc664a8d52fb3dde50da3
Randomchair22 <169164624+Randomchair22@users.noreply.github.com>
2026-01-25 21:34:59 -0500

    Fix Armies Case for Kingdom Command

--- BannerlordTwitch/BLTAdoptAHero/Actions/KingdomManagement.cs
+++ BannerlordTwitch/BLTAdoptAHero/Actions/KingdomManagement.cs
@@ -440,7 +440,7 @@ namespace BLTAdoptAHero.Actions
                 case "stats":
                     HandleStatsCommand(settings, adoptedHero, onSuccess, onFailure);
                     break;
-                case "Armies":
+                case "armies":
                     HandleArmiesCommand(settings, adoptedHero, onSuccess, onFailure);
                     break;
                 case "tax":
