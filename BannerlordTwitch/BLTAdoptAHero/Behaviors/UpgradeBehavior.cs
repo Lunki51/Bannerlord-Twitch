3658fc05f0dc27e50f4e953e545c4316c52a16a2
Randomchair22 <169164624+Randomchair22@users.noreply.github.com>
2026-02-27 17:53:51 -0500

    Fix Garrison Capacity

--- BannerlordTwitch/BLTAdoptAHero/Behaviors/UpgradeBehavior.cs
+++ BannerlordTwitch/BLTAdoptAHero/Behaviors/UpgradeBehavior.cs
@@ -301,7 +301,7 @@ namespace BLTAdoptAHero
         {
             var garrison = settlement?.Town?.GarrisonParty;
             if (garrison == null) return false;
-            if (garrison.MemberRoster.TotalManCount >= (garrison.Party.PartySizeLimit + GetTotalGarrisonCapacityBonus(settlement))) return false;
+            if (garrison.MemberRoster.TotalManCount >= garrison.Party.PartySizeLimit) return false;
 
             var troop = GetTroopForCulture(settlement.Culture, tree, tier);
             if (troop == null) return false;
@@ -314,7 +314,7 @@ namespace BLTAdoptAHero
         private Settlement GetRandomGarrisonSettlementForClan(Clan clan)
             => clan?.Settlements
                    .Where(s => s.Town?.GarrisonParty != null &&
-                               s.Town.GarrisonParty.MemberRoster.TotalManCount < (s.Town.GarrisonParty.Party.PartySizeLimit + GetTotalGarrisonCapacityBonus(s)))
+                               s.Town.GarrisonParty.MemberRoster.TotalManCount < s.Town.GarrisonParty.Party.PartySizeLimit)
                    .SelectRandom();
 
         private void RunAccumulation(string key, float amount, Func<bool> trySpawn)
